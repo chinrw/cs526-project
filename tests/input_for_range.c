5653c63db02b5fd6a4c3f95e8e2ece6a8abe6f97
yuzhuow2 <yuzhuow2@illinois.edu>
2023-05-06 14:54:01 -0500

    Add the test for CallInst, StoreInst, ReturnInst

--- tests/input_for_range.c
+++ tests/input_for_range.c
@@ -1,9 +1,11 @@
+// #include <cstdint>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdbool.h>
 
 uint64_t wow = 234234324234;
 uint64_t wow2;
+uint64_t wow3;
 char string_test[] = "asdfasdf";
 uint64_t* uninit_array = NULL;
 uint64_t init_array[10] = {1,2,3,4,5,6,7};
@@ -55,11 +57,23 @@ uint64_t testPHINode(uint64_t a, uint64_t b, uint64_t c) {
     return result;
 }
 
+// test CallInst
+void updateWow3(uint64_t value) {
+  wow3 = value;
+}
+
+// test ReturnInst
+uint64_t testReturnInst(uint64_t value) {
+  return value * 2;
+}
+
+
 int main() {
   uint64_t a = UINT64_MAX - 10;
   uint32_t b = 5;
   uint32_t c = 2;
   uint64_t d = UINT64_MAX / 2;
+  uint64_t callInstValue = 10;
 
   uint64_t result = calculate(a, b, c, d);
   printf("Result: %jd\n", result);
@@ -73,5 +87,15 @@ int main() {
   printf("SExt castInst Result: %jd\n", castSExtResult);
   uint64_t phiNodeResult = testPHINode(a, b, c);
   printf("PHINode Result: %jd\n", phiNodeResult);
+  updateWow3(callInstValue);
+  printf("CallInst - wow3 updated to: %jd\n", wow3);
+
+  // Test StoreInst
+  wow2 = wow * 2;
+  printf("StoreInst - wow2 assigned: %jd\n", wow2);
+
+  // Test ReturnInst
+  uint64_t returnInstValue = testReturnInst(callInstValue);
+  printf("ReturnInst - returned value: %jd\n", returnInstValue);
   return 0;
 }
