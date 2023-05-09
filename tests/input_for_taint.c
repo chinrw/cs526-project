74e8de63b7140a5ca5caf27869aa037ad00f9871
Ruowen Qin <chinqrw@gmail.com>
2023-05-09 17:43:20 -0500

    Merge taint input and range input

--- tests/input_for_taint.c
+++ tests/input_for_taint.c
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -115,6 +117,93 @@ void test_case_extended() {
   printf("\n");
 }
 
+uint64_t wow = 234234324234;
+uint64_t wow2;
+uint64_t wow3;
+char string_test[] = "asdfasdf";
+uint64_t *uninit_array = NULL;
+uint64_t init_array[10] = {1, 2, 3, 4, 5, 6, 7};
+
+uint64_t add(uint64_t a, uint32_t b) { return a + b; }
+
+uint64_t subtract(uint64_t a, uint32_t b) { return a - b; }
+
+uint64_t multiply(uint32_t a, uint64_t b) { return a * b; }
+
+uint64_t divide(uint64_t a, uint32_t b) { return a / b; }
+
+uint64_t xor (uint64_t a, uint32_t b) { return a ^ b; }
+
+    uint64_t calculate(uint64_t a, uint32_t b, uint32_t c, uint64_t d) {
+  uint64_t sum = add(a, b);
+  uint64_t difference = subtract(sum, c);
+  uint64_t product = multiply(c, d);
+  uint64_t quotient = divide(product, b);
+  return xor(quotient, difference);
+}
+// test SelectInst
+uint64_t selectInst(uint64_t a, uint64_t b, bool condition) {
+  return condition ? a : b;
+}
+
+// test CastInst (Trunc)
+uint64_t castInst(uint64_t a) {
+  uint32_t truncated = (uint32_t)a;
+  return (uint64_t)truncated;
+}
+uint64_t testCastZExt(uint32_t a) { return (uint64_t)a; }
+
+uint64_t testCastSExt(int32_t a) { return (int64_t)a; }
+
+// test PHINode
+uint64_t testPHINode(uint64_t a, uint64_t b, uint64_t c) {
+  uint64_t result;
+  if (a < b) {
+    result = a * c;
+  } else {
+    result = b * c;
+  }
+  return result;
+}
+
+// test CallInst
+void updateWow3(uint64_t value) { wow3 = value; }
+
+// test ReturnInst
+uint64_t testReturnInst(uint64_t value) { return value * 2; }
+
+void range() {
+
+  uint64_t a = UINT64_MAX - 10;
+  uint32_t b = 5;
+  uint32_t c = 2;
+  uint64_t d = UINT64_MAX / 2;
+  uint64_t callInstValue = 10;
+
+  uint64_t result = calculate(a, b, c, d);
+  printf("Result: %jd\n", result);
+  uint64_t selectInstResult = selectInst(a, b, true);
+  printf("selectInst Result: %jd\n", selectInstResult);
+  uint64_t castTruncResult = castInst(a);
+  printf("Truncate castInst Result: %jd\n", castTruncResult);
+  uint64_t castZExtResult = testCastZExt(b);
+  printf("ZExt castInst Result: %jd\n", castZExtResult);
+  uint64_t castSExtResult = testCastSExt(b);
+  printf("SExt castInst Result: %jd\n", castSExtResult);
+  uint64_t phiNodeResult = testPHINode(a, b, c);
+  printf("PHINode Result: %jd\n", phiNodeResult);
+  updateWow3(callInstValue);
+  printf("CallInst - wow3 updated to: %jd\n", wow3);
+
+  // Test StoreInst
+  wow2 = wow * 2;
+  printf("StoreInst - wow2 assigned: %jd\n", wow2);
+
+  // Test ReturnInst
+  uint64_t returnInstValue = testReturnInst(callInstValue);
+  printf("ReturnInst - returned value: %jd\n", returnInstValue);
+}
+
 int main(int argc, char *argv[]) {
   // use get user input
   printf("Hello\n");
@@ -169,6 +258,8 @@ int main(int argc, char *argv[]) {
     printf("Extended Test case %d\n", i + 1);
     test_case_extended();
   }
+
+  range();
 
   return EXIT_SUCCESS;
 }
